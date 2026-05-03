b9bfb969bb01df0310952c2d8568fd788a86bcd7
cstewart2010 <cstewart2010@users.noreply.github.com>
2026-05-03 03:47:57 +0000

    deploy: 9e76a431a825b04011e3a2ebcc710ad31a5ba9cf

---
+++ js/taxBrackets.js
@@ -0,0 +1,214 @@
+const brackets = [
+    {
+      "friendlyName": "Single",
+      "status": 1,
+      "standardDeduction": 14600,
+      "capitalGainsDeduction": 47025,
+      "rates": [
+        {
+          "taxRate": 10,
+          "lowerBound": 0,
+          "upperBound": 11600
+        },
+        {
+          "taxRate": 12,
+          "lowerBound": 11600,
+          "upperBound": 47150
+        },
+        {
+          "taxRate": 22,
+          "lowerBound": 47150,
+          "upperBound": 100525
+        },
+        {
+          "taxRate": 24,
+          "lowerBound": 100525,
+          "upperBound": 191950
+        },
+        {
+          "taxRate": 32,
+          "lowerBound": 191950,
+          "upperBound": 243725
+        },
+        {
+          "taxRate": 35,
+          "lowerBound": 243725,
+          "upperBound": 609350
+        },
+        {
+          "taxRate": 37,
+          "lowerBound": 609350
+        }
+      ],
+      "capitalGainsRates":[
+        {
+          "taxRate": 15,
+          "lowerBound": 47025,
+          "upperBound": 518900
+        },
+        {
+          "taxRate": 20,
+          "lowerBound": 518900,
+        },
+      ]
+    },
+    {
+      "friendlyName": "Married, filing jointly",
+      "status": 2,
+      "standardDeduction": 29200,
+      "capitalGainsDeduction": 94050,
+      "rates": [
+        {
+          "taxRate": 10,
+          "lowerBound": 0,
+          "upperBound": 23200
+        },
+        {
+          "taxRate": 12,
+          "lowerBound": 23200,
+          "upperBound": 94300
+        },
+        {
+          "taxRate": 22,
+          "lowerBound": 94300,
+          "upperBound": 201050
+        },
+        {
+          "taxRate": 24,
+          "lowerBound": 201050,
+          "upperBound": 383900
+        },
+        {
+          "taxRate": 32,
+          "lowerBound": 383900,
+          "upperBound": 487450
+        },
+        {
+          "taxRate": 35,
+          "lowerBound": 487450,
+          "upperBound": 731200
+        },
+        {
+          "taxRate": 37,
+          "lowerBound": 731200
+        }
+      ],
+      "capitalGainsRates":[
+        {
+          "taxRate": 15,
+          "lowerBound": 94050,
+          "upperBound": 583750
+        },
+        {
+          "taxRate": 20,
+          "lowerBound": 583750,
+        },
+      ]
+    },
+    {
+      "friendlyName": "Married, filing separately",
+      "status": 3,
+      "standardDeduction": 14600,
+      "capitalGainsDeduction": 47025,
+      "rates": [
+        {
+          "taxRate": 10,
+          "lowerBound": 0,
+          "upperBound": 11600
+        },
+        {
+          "taxRate": 12,
+          "lowerBound": 11600,
+          "upperBound": 47150
+        },
+        {
+          "taxRate": 22,
+          "lowerBound": 47150,
+          "upperBound": 100525
+        },
+        {
+          "taxRate": 24,
+          "lowerBound": 100525,
+          "upperBound": 191950
+        },
+        {
+          "taxRate": 32,
+          "lowerBound": 191950,
+          "upperBound": 243725
+        },
+        {
+          "taxRate": 35,
+          "lowerBound": 243725,
+          "upperBound": 365600
+        },
+        {
+          "taxRate": 37,
+          "lowerBound": 365600
+        }
+      ],
+      "capitalGainsRates":[
+        {
+          "taxRate": 15,
+          "lowerBound": 47025,
+          "upperBound": 291850
+        },
+        {
+          "taxRate": 20,
+          "lowerBound": 291850,
+        },
+      ]
+    },
+    {
+      "friendlyName": "Head of household",
+      "status": 4,
+      "standardDeduction": 21900,
+      "capitalGainsDeduction": 63000,
+      "rates": [
+        {
+          "taxRate": 10,
+          "lowerBound": 0,
+          "upperBound": 16550
+        },
+        {
+          "taxRate": 12,
+          "lowerBound": 16550,
+          "upperBound": 63100
+        },
+        {
+          "taxRate": 22,
+          "lowerBound": 63100,
+          "upperBound": 100500
+        },
+        {
+          "taxRate": 24,
+          "lowerBound": 100500,
+          "upperBound": 191950
+        },
+        {
+          "taxRate": 32,
+          "lowerBound": 191950,
+          "upperBound": 243700
+        },
+        {
+          "taxRate": 35,
+          "lowerBound": 243700,
+          "upperBound": 609350
+        },
+        {
+          "taxRate": 37,
+          "lowerBound": 609350
+        }
+      ],
+      "1160":[
+        {
+          "taxRate": 15,
+          "lowerBound": 63000,
+          "upperBound": 551350
+        },
+        {
+          "taxRate": 20,
+          "lowerBound": 551350,
+        },
+      ]
+    }
+  ];
---
+++ js/taxEstimator.js
@@ -0,0 +1,185 @@
+/**
+ * @typedef TaxRateData
+ * @prop {number} taxRate
+ * @prop {number} lowerBound
+ * @prop {number?} upperBound
+ * @typedef BracketData
+ * @prop {string} friendlyName
+ * @prop {number} status
+ * @prop {number} standardDeduction
+ * @prop {number} capitalGainsDeduction
+ * @prop {TaxRateData[]} rates
+ * @prop {TaxRateData[]} capitalGainsRates
+ */
+
+const FICA_RATE = .0765;
+const INCOME_INPUT_SELECTOR = "#income";
+const SHORT_GAINS_SELECTOR = "#short-gains";
+const LONG_GAINS_SELECTOR = "#long-gains";
+const RETIREMENT_INPUT_SELECTOR = "#retirement";
+const HSA_INPUT_SELECTOR = "#hsa";
+const PAYROLL_INPUT_SELECTOR = "#payroll";
+const INFANT_INPUT_SELECTOR = "#infant";
+const YOUNG_INPUT_SELECTOR = "#young";
+const ADULT_INPUT_SELECTOR = "#adult";
+const INCOME_TAX_BURDEN_TEXT_SELECTOR = "#income-tax-burden";
+const CAPITAL_GAINS_TAX_BURDEN_TEXT_SELECTOR = "#capital-gains-tax-burden";
+const FICA_BURDEN_TEXT_SELECTOR = "#fica-burden";
+const FILING_STATUS_SELECT_SELECTOR = "#filing-status";
+const CREDIT_TEXT_SELECTOR = "#credits";
+const EFFECTIVE_TAX_RATE_TEXT_SELECTOR = "#effective-tax-rate";
+
+/**
+ *
+ * @param {string} selector
+ * @returns {string}
+ */
+const getValueFromElement = (selector) => {
+    return document.querySelector(selector).value;
+}
+
+/**
+ *
+ * @param {string} selector
+ * @param {string} text
+ */
+const setElementText = (selector, text) => {
+    document.querySelector(selector).innerHTML = text;
+}
+
+/**
+ *
+ * @returns {BracketData}
+ */
+const getBracketData = () => {
+    const inputElement = document.querySelector(FILING_STATUS_SELECT_SELECTOR);
+    const index = getTransformedIntInput(
+        inputElement,
+        input => input > 0 && input <= brackets.length,
+        input => input - 1
+    );
+
+    if (index >= 0){
+        return brackets[index]
+    }
+
+    return null;
+}
+
+/**
+ *
+ * @param {BracketData} bracket
+ */
+const getTaxDeductions = (bracket) => {
+    const retirementInputElement = document.querySelector(RETIREMENT_INPUT_SELECTOR);
+    const hsaInputElement = document.querySelector(HSA_INPUT_SELECTOR);
+    const payrollDeductionsInputElement = document.querySelector(PAYROLL_INPUT_SELECTOR);
+    let totalDeductions = bracket.standardDeduction;
+    totalDeductions += getTransformedIntInput(
+        retirementInputElement,
+        input => input > 0 && input <= 23000,
+        input => input
+    );
+    totalDeductions += getTransformedIntInput(
+        hsaInputElement,
+        input => input > 0 && input <= 8300,
+        input => input
+    );
+    totalDeductions += getTransformedIntInput(
+        payrollDeductionsInputElement,
+        input => input > 0,
+        input => input * 12
+    );
+
+    return totalDeductions;
+}
+
+const getTaxCredits = () => {
+    const infantDependentsInputElement = document.querySelector(INFANT_INPUT_SELECTOR);
+    const youngDependentsInputElement = document.querySelector(YOUNG_INPUT_SELECTOR);
+    const adultDependentsInputElement = document.querySelector(ADULT_INPUT_SELECTOR);
+    let totalCredits = 0;
+    totalCredits += getTransformedIntInput(
+        infantDependentsInputElement,
+        input => input > 0,
+        input => input * 3600
+    );
+    totalCredits += getTransformedIntInput(
+        youngDependentsInputElement,
+        input => input > 0,
+        input => input * 3000
+    );
+    totalCredits += getTransformedIntInput(
+        adultDependentsInputElement,
+        input => input > 0,
+        input => input * 500
+    );
+
+    return totalCredits;
+}
+
+/**
+ *
+ * @param {TaxRateData[]} rates
+ * @param {number?} taxableIncome
+ */
+const getTaxBurden = (rates, taxableIncome) => {
+    const actualTaxableIncome = taxableIncome > 0 ? taxableIncome : 0;
+    let taxBurden = 0;
+    if (actualTaxableIncome == 0)
+    {
+        return taxBurden;
+    }
+    for (const rate of rates){
+        if (!rate.upperBound || rate.upperBound > taxableIncome){
+            taxBurden += getCleanMathOutput(rate.taxRate, taxableIncome, rate.lowerBound);
+            break;
+        }
+
+        taxBurden += getCleanMathOutput(rate.taxRate, rate.upperBound, rate.lowerBound);
+    }
+
+    return taxBurden === 0 ? 1 : Math.ceil(taxBurden);
+}
+
+/**
+ *
+ * @param {number} rate
+ * @param {number} largerNumber
+ * @param {number} smallNumber
+ */
+const getCleanMathOutput = (rate, largerNumber, smallNumber) => {
+    return rate * (largerNumber - smallNumber) / 100;
+}
+
+/**
+ *
+ * @param {number} income
+ * @param {number} taxBurden
+ * @returns
+ */
+const getEffectiveTaxRate = (income, taxBurden) => {
+    if (income === 0){
+        return 0;
+    }
+    return taxBurden / income * 100;
+}
+
+/**
+ *
+ * @param {HTMLInputElement} inputElement
+ * @param {(input: number) => boolean} validator
+ * @param {(input: number) => number} transformation
+ */
+const getTransformedIntInput = (
+    inputElement,
+    validator,
+    transformation
+) => {
+    const input = Number.parseInt(inputElement.value);
+    if (validator(input)){
+        return transformation(input)
+    }
+
+    return 0;
+}
